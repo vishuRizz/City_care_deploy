fa938951b0f94a6d345ca006b8a9cd4884db4d9d
vishu pratap <vishurizz01@gmail.com>
2024-10-07 13:14:08 +0530

    changing cors

--- Server_side/index.js
+++ Server_side/index.js
@@ -5,7 +5,7 @@ const rootRouter = require("./routes/index");
 const app = express();
 
 app.use(cors({
-  origin: ["https://deploy-mern-1whq.vercel.app"],
+  origin: ["https://city-care.vercel.app/"],
   methods: ["POST", "GET"],
   credentials: true,
 }));
