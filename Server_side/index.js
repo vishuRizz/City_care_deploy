b4ca76ee6405c748294379024317980d17c86ea4
vishu pratap <vishurizz01@gmail.com>
2024-10-07 11:41:49 +0530

    removing vulnaribility

--- Server_side/index.js
+++ Server_side/index.js
@@ -4,7 +4,11 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
+}));
 app.use(express.json());
  
 app.use("/api/v1", rootRouter);
